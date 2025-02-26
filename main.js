d1b7cb238f64366adf19acf61a1a7cc2a07b7f20
Lee Hyeran <bonita7437@hanmail.net>
2025-02-27 01:21:21 +0900

    second commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const API_KEY = `d560ddbd071c4173a2387aabfcb6084f`;
-let news = [];
+let newsList = [];
 
 const getLatestNews = async () => {
   const url = new URL(
@@ -7,8 +7,41 @@ const getLatestNews = async () => {
   );
   const response = await fetch(url);
   const data = await response.json();
-  news = data.articles;
-  console.log("aaa", news);
+  newsList = data.articles;
+  render();
+  console.log("aaa", newsList);
+};
+
+const render = () => {
+  const newsHTML = newsList
+    .map(
+      (news) => `<div class="row news">
+          <div class="col-lg-4">
+            <img class="news-img-size" src="${
+              news.urlToImage
+            }"onerror="this.onerror=null; this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU';"/>
+          </div>
+          <div class="col-lg-8">
+            <h2>${news.title}</h2>
+          <p>${
+            news.summary == null || news.summary == ""
+              ? "내용없음"
+              : news.summary.length > 200
+              ? news.summary.substring(0, 200) + "..."
+              : news.summary
+          }</p>
+            <p>${news.description ? news.description : "설명 없음"}</p>
+
+            <div>${news.rights || "no source"} · ${moment(
+        news.publishedAt
+      ).fromNow()}</div>
+
+          </div>
+        </div>`
+    )
+    .join("");
+
+  document.getElementById("news-board").innerHTML = newsHTML;
 };
 
 getLatestNews();
