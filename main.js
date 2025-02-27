121afa2b67601634e6a42c5a29cf2a995d867f5d
Lee Hyeran <bonita7437@hanmail.net>
2025-02-28 01:38:24 +0900

    27 commit

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 const API_KEY = `d560ddbd071c4173a2387aabfcb6084f`;
 let newsList = [];
 
+const openNav = () => {
+  const sideNav = document.querySelector(".side-menu");
+  sideNav.style.width = "250px";
+};
+
+const closeNav = () => {
+  const sideNav = document.querySelector(".side-menu");
+  sideNav.style.width = "0";
+};
+
+const openSearchBox = () => {
+  let inputArea = document.getElementById("input-area");
+  // 클릭 시 검색창을 토글하기 위해 스타일 변경
+  if (inputArea.style.display === "inline") {
+    inputArea.style.display = "none";
+  } else {
+    inputArea.style.display = "inline";
+  }
+};
+
+// 아이콘 클릭 시 검색창을 보이도록 하는 클래스 추가
+const toggleSearchBox = () => {
+  document.body.classList.toggle("search-visible");
+};
+
 const getLatestNews = async () => {
   const url = new URL(
     `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`
